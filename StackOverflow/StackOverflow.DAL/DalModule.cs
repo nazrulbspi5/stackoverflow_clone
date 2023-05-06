9cdf87f073636f385f094716122c5d3ba8ed7449
nazrulbspi5 <nazrulbspi5@gmail.com>
2023-05-06 22:22:45 +0600

    Added DalModule class for dependency injection

---
+++ StackOverflow/StackOverflow.DAL/DalModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+
+namespace StackOverflow.DAL
+{
+    public class DalModule:Module
+    {
+        private readonly string _connectionString;
+        public DalModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            //For Migration
+            builder.RegisterType<ApplicationDbContext>().AsSelf().
+                WithParameter("connectionString", _connectionString).
+               //WithParameter("migrationAssemblyName", _migrationAssemblyName).
+                InstancePerLifetimeScope();
+            base.Load(builder);
+        }
+    }
+}

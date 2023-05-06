41a38f0926e3698d4649eadd5c969785c858e0bc
nazrulbspi5 <nazrulbspi5@gmail.com>
2023-05-07 03:09:41 +0600

    Added identity services

---
+++ StackOverflow/StackOverflow.Services/ServiceModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using StackOverflow.DAL.NHibernate;
+using StackOverflow.Services.Authentication;
+
+
+namespace StackOverflow.Services
+{
+    public class ServiceModule:Module
+    {
+        private readonly string _connectionString;
+        public ServiceModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SessionManagerFactory>().As<ISessionManagerFactory>().
+                WithParameter("connectionString", _connectionString).
+                InstancePerLifetimeScope();
+
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerLifetimeScope();
+            base.Load(builder);
+        }
+    }
+}
---
+++ StackOverflow/StackOverflow.Services/Services/Authentication/ApplicationRoleManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using StackOverflow.DAL.Entities.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace StackOverflow.Services.Authentication
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole> store,
+            IEnumerable<IRoleValidator<ApplicationRole>> roleValidators,ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors,ILogger<RoleManager<ApplicationRole>> logger):base(store,roleValidators,keyNormalizer,errors,logger)
+        {
+
+        }
+    }
+}
